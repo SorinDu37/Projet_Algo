a47dc999efae4731badb4030b68e34f1f4d5b30e
blaise <blaise.muryango@ulb.be>
2026-04-22 17:16:49 +0200

    ajout des commentaires

--- src/dblp/Main.java
+++ src/dblp/Main.java
@@ -1,23 +1,32 @@
 package dblp;
 
-import dblp.core.Tache;
 import dblp.taches.Tache1;
 import dblp.taches.Tache2;
 
+/**
+ * Point d'entrée du programme. Lance successivement la Tâche 1 (Union-Find)
+ * puis la Tâche 2 (graphe orienté + Tarjan) sur le fichier DBLP.
+ */
 public class Main {
     public static void main(String[] args) {
         String xml = "lib/dblp-2026-01-01.xml.gz";
         String dtd = "lib/dblp.dtd";
 
+        // Désactiver les limites d'expansion d'entités XML (nécessaire pour le DTD de DBLP)
+        System.setProperty("jdk.xml.entityExpansionLimit", "0");
+        System.setProperty("jdk.xml.totalEntitySizeLimit", "0");
+        System.setProperty("jdk.xml.maxGeneralEntitySizeLimit", "0");
+        System.setProperty("jdk.xml.maxParameterEntitySizeLimit", "0");
+
         try {
             System.out.println("LANCEMENT DU PROGRAMME D'ANALYSE DBLP\n");
 
-            Tache tache1 = new Tache1(xml, dtd);
+            Tache1 tache1 = new Tache1(xml, dtd);
             tache1.executer();
 
             System.out.println("\n=========================================\n");
 
-            Tache tache2 = new Tache2(xml, dtd);
+            Tache2 tache2 = new Tache2(xml, dtd);
             tache2.executer();
 
             System.out.println("\nTOUTES LES TÂCHES SONT TERMINÉES !");
